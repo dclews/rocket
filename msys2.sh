9024546b5c4f2e90558f76fed83c26528ed71e47
xClueless <clueless_@live.co.uk>
2016-09-08 19:13:59 +0100

    MSYS2 setup script.

---
+++ msys2.sh
@@ -0,0 +1,16 @@
+echo export PATH="$USERPOFILE/.cargo/bin:$PATH" > ~/.bash_profile
+export PATH="$USERPOFILE/.cargo/bin:$PATH"
+
+which cargo
+if [ $? -ne 0 ]; then
+	curl https://sh.rustup.rs -sSf | sh
+fi
+
+which xargo
+if [ $? -ne 0 ]; then
+	cargo install xargo
+fi
+
+git clone https://github.com/dclews/rocket.git
+cd rocket
+cargo install
